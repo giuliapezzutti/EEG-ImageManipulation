b62d9c76dbc20f1651649dce5791889ae979175f
giuliapezzutti <giulia.pezzutti@studenti.unipd.it>
2022-06-08 11:08:13 +0200

    LDA frontal mean amplitude

--- src/main-lda.py
+++ src/main-lda.py
@@ -1,6 +1,8 @@
 import glob
+import json
 import pickle
 
+import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
@@ -9,6 +11,9 @@ if __name__ == '__main__':
     paths = [path.replace('\\', '/') for path in paths if '_data' in path]
 
     codes = [(path.rsplit('_', 1)[0]).rsplit('/', 1)[1] for path in paths]
+
+    input_info = json.load(open('../data/eeg/info.json'))
+    rois = input_info['rois']
 
     participant_data = pd.read_csv('../data/form-results/form-results.csv')
     participant_data = participant_data[['code', 'gender']]
@@ -19,5 +24,23 @@ if __name__ == '__main__':
             data = pickle.load(f)
         with open(paths[idx].replace('_data', '_info'), 'rb') as f:
             info = pickle.load(f)
+        with open(paths[idx].replace('_data', '_labels'), 'rb') as f:
+            labels = pickle.load(f)
+            conditions = list(set(labels))
 
         g = participant_data.loc[participant_data['code'] == code]['gender'].values[0]
+
+        # calculate mean frontal amplitude in 300-600ms
+
+        frontal_indexes = np.where(np.in1d(np.array(info['channels']), np.array(rois['frontal'])))[0]
+        start = int((0.3 - info['tmin']) * info['fs'])
+        end = int((0.6 - info['tmin']) * info['fs'])
+
+        frontal_data = data[:, frontal_indexes, start:end]
+        frontal_amplitude = np.mean(frontal_data, axis=-1)
+        frontal_amplitude = np.mean(frontal_amplitude, axis=-1)
+
+        pd_data = np.vstack((np.array(labels), frontal_amplitude)).T
+        print(pd_data.shape)
+        print(pd_data)
+        exit(1)
