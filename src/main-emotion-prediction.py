f55dbb4c224396316d503544083359fc1998aea4
giuliapezzutti <giulia.pezzutti@studenti.unipd.it>
2022-07-06 16:56:26 +0200

    CSP with MNE-torch

--- src/main-emotion-prediction.py
+++ src/main-emotion-prediction.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from mne.decoding import (CSP)
 
 from EEGAnalysis import *
-from src.models.EEGModels import EEGNet
+from src.models.EEGModels import EEGNet, DeepConvNet
 import tensorflow as tf
 
 if __name__ == '__main__':
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         if code not in codes_ratings:
             continue
 
-        if code == 'krki20' or code == 'nipe10' or code == 'maba09' or code == 'soze31' or code == 'dino02' or code == 'ervi22':
+        if code == 'krki20' or code == 'nipe10' or code == 'maba09' or code == 'soze31' or code == 'dino02':
             continue
 
         print(code)
@@ -61,34 +61,36 @@ if __name__ == '__main__':
             img_name = labels[idx].split('/')[0]
             ratings.append(data_ratings.loc[data_ratings['img_name'] == img_name][['valence', 'arousal']].values[0])
 
-        encoding = []
-        for labels in ratings:
-            valence = labels[0]
-            arousal = labels[1]
-
-            if (np.square(valence) + np.square(arousal)) <= np.square(threshold):
-                encoding.append(0)
-            elif valence > 0 and arousal > 0:
-                encoding.append(1)
-            elif valence > 0 >= arousal:
-                encoding.append(3)
-            elif valence <= 0 < arousal:
-                encoding.append(2)
-            elif valence <= 0 and arousal <= 0:
-                encoding.append(4)
-
-        print(data.shape)
-        print(np.array(encoding).shape)
-
-        csp = pyriemann.spatialfilters.CSP()
-        csp.fit(np.array(data), np.array(encoding))
-        csp_data = csp.transform(data)
-        print(csp_data)
-        exit(1)
+        # encoding = []
+        # for labels in ratings:
+        #     valence = labels[0]
+        #     arousal = labels[1]
+        #
+        #     if (np.square(valence) + np.square(arousal)) <= np.square(threshold):
+        #         encoding.append(0)
+        #     elif valence > 0 and arousal > 0:
+        #         encoding.append(1)
+        #     elif valence > 0 >= arousal:
+        #         encoding.append(3)
+        #     elif valence <= 0 < arousal:
+        #         encoding.append(2)
+        #     elif valence <= 0 and arousal <= 0:
+        #         encoding.append(4)
+        #
+        # print(data.shape)
+        # print(np.array(encoding).shape)
+        #
+        # csp = CSP()
+        # csp.fit(np.array(data), np.array(encoding))
+        # csp_data = csp.transform(data)
+        # print(csp_data)
+        # exit(1)
 
         info_dataset.extend(form)
         signal_dataset.extend(data)
         label_dataset.extend(np.array(ratings))
+
+        continue
 
     info_dataset = np.array(info_dataset, dtype=object)
     signal_dataset = np.array(signal_dataset, dtype=float)
@@ -106,9 +108,10 @@ if __name__ == '__main__':
         valence = labels[0]
         arousal = labels[1]
 
-        if (np.square(valence) + np.square(arousal)) <= np.square(threshold):
-            label_binary_dataset.append([0, 0, 1])
-        elif valence > 0 and arousal > 0:
+        # if (np.square(valence) + np.square(arousal)) <= np.square(threshold):
+        #     label_binary_dataset.append([0, 0, 1])
+        # else:
+        if valence > 0 and arousal > 0:
             label_binary_dataset.append([1, 1, 0])
         elif valence > 0 >= arousal:
             label_binary_dataset.append([1, 0, 0])
@@ -117,7 +120,18 @@ if __name__ == '__main__':
         elif valence <= 0 and arousal <= 0:
             label_binary_dataset.append([0, 0, 0])
 
-    label_binary_dataset = np.array(label_binary_dataset, dtype=float)
+        # if (np.square(valence) + np.square(arousal)) <= np.square(threshold):
+        #     label_binary_dataset.append(0)
+        # elif valence > 0 and arousal > 0:
+        #     label_binary_dataset.append(1)
+        # elif valence > 0 >= arousal:
+        #     label_binary_dataset.append(3)
+        # elif valence <= 0 < arousal:
+        #     label_binary_dataset.append(2)
+        # elif valence <= 0 and arousal <= 0:
+        #     label_binary_dataset.append(4)
+
+    label_binary_dataset = np.array(label_binary_dataset, dtype=float)[:, 0]
 
     print('Dataset ready for the training!\n')
 
@@ -125,21 +139,21 @@ if __name__ == '__main__':
                                                                                                info_dataset,
                                                                                                label_binary_dataset,
                                                                                                test_size=0.2)
-    val_data, test_data, val_info, test_info, val_labels, test_labels = train_test_split(test_data, test_info,
-                                                                                         test_labels, test_size=0.5)
+    # val_data, test_data, val_info, test_info, val_labels, test_labels = train_test_split(test_data, test_info,
+    #                                                                                      test_labels, test_size=0.5)
 
     batch_size = 16
     num_epochs = 20
 
     input_shape = (train_data[0].shape[0], train_data[0].shape[1])
 
-    model = EEGNet(nb_classes=3, Chans=input_shape[0], Samples=input_shape[1])
+    model = DeepConvNet(nb_classes=1, Chans=input_shape[0], Samples=input_shape[1])
     model.summary()
 
-    opt = tf.keras.optimizers.Adam(learning_rate=0.01)
-    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'])
 
-    history = model.fit(x=train_data[:], y=train_labels[:], validation_data=(val_data, val_labels),
+    history = model.fit(x=train_data[:], y=train_labels[:], validation_split=0.2,
                         batch_size=batch_size, epochs=num_epochs)
 
     # Extract labels of test set, predict them with the model
@@ -160,28 +174,28 @@ if __name__ == '__main__':
 
     # Plot of loss-accuracy and ROC
 
-    fig, axs = plt.subplots(2, 2)
+    fig, axs = plt.subplots(2, 1)
     fig.suptitle('Loss, accuracy and ROC')
     # Plot loss
-    axs[0, 0].plot(history.history['loss'], label='Train loss')
-    axs[0, 0].plot(history.history['val_loss'], label='Val loss')
-    axs[0, 0].legend()
-    axs[0, 0].set_xlabel('Epoch')
-    axs[0, 0].set_ylabel('Loss')
-    axs[0, 0].set_title('Loss')
+    axs[0].plot(history.history['loss'], label='Train loss')
+    axs[0].plot(history.history['val_loss'], label='Val loss')
+    axs[0].legend()
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_ylabel('Loss')
+    axs[0].set_title('Loss')
     # Plot accuracy
-    axs[1, 0].plot(history.history['accuracy'], label='Train accuracy')
-    axs[1, 0].plot(history.history['val_accuracy'], label='Val accuracy')
-    axs[1, 0].legend()
-    axs[1, 0].set_xlabel('Epoch')
-    axs[1, 0].set_ylabel('Accuracy')
-    axs[1, 0].set_title('Accuracy')
-    fpr, tpr, _ = roc_curve(test_labels, test_est_classes)
-    roc_auc = auc(fpr, tpr)
-    # Plot ROC when only 1 label is present
-    axs[0, 1].plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-    axs[0, 1].plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    axs[0, 1].set_xlabel('False Positive Rate')
-    axs[0, 1].set_ylabel('True Positive Rate')
-    axs[0, 1].set_title('ROC')
-    plt.show()
+    axs[1].plot(history.history['accuracy'], label='Train accuracy')
+    axs[1].plot(history.history['val_accuracy'], label='Val accuracy')
+    axs[1].legend()
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_ylabel('Accuracy')
+    axs[1].set_title('Accuracy')
+    # fpr, tpr, _ = roc_curve(test_labels, test_est_classes)
+    # roc_auc = auc(fpr, tpr)
+    # # Plot ROC when only 1 label is present
+    # axs[0, 1].plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    # axs[0, 1].plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    # axs[0, 1].set_xlabel('False Positive Rate')
+    # axs[0, 1].set_ylabel('True Positive Rate')
+    # axs[0, 1].set_title('ROC')
+    # plt.show()
